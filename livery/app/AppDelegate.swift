4fe9bd1f3b585e3039aa7f40d0e0953ebea2b938
codingschoolchajari <88979885+codingschoolchajari@users.noreply.github.com>
2026-08-18 00:00:05 -0300

    Crashlytics

--- livery/app/AppDelegate.swift
+++ livery/app/AppDelegate.swift
@@ -24,10 +24,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         FirebaseApp.configure()
         Crashlytics.crashlytics().setCrashlyticsCollectionEnabled(true)
 
-        if ProcessInfo.processInfo.arguments.contains("-crashlyticsTestCrash") {
-            fatalError("Crashlytics test crash")
-        }
-
         // Google Maps
         if let apiKey = Bundle.main.object(
             forInfoDictionaryKey: "GOOGLE_MAPS_API_KEY"
